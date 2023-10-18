15ae152c178e2e2e1d0bcebfeae41e5f06ecb295
priyank-el <priyank.a@elaunchinfotech.in>
2023-10-18 17:19:27 +0530

    feat:implement product view page

---
+++ client/src/pages/product/ViewproductData.jsx
@@ -0,0 +1,21 @@
+import { useLocation } from "react-router-dom"
+
+function ViewproductData() {
+
+    const location = useLocation()
+    const data = location.state
+
+  return (
+    <>
+        <div className="flex justify-center items-center">
+            <div>
+                <img className="h-2/4 w-2/4 mx-auto rounded-md" src={`http://localhost:3000/images/${data.data.image}`} alt="image comes here" />
+            <p className="text-center text-5xl font-extrabold mt-5">{data.data.productName}</p>
+            <p className="text-center mt-2">{data.data.productDescription}</p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default ViewproductData
