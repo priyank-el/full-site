2425a05158f35e34e0902d60be508c3efe8db61a
priyank-el <priyank.a@elaunchinfotech.in>
2023-10-19 09:55:06 +0530

    fix:Implement antd pagination

--- client/src/pages/product/allProducts.jsx
+++ client/src/pages/product/allProducts.jsx
@@ -3,8 +3,7 @@ import { useContext, useEffect, useState } from "react"
 import { UserName } from "../../providers/ContextProvider"
 import { useNavigate } from "react-router-dom"
 import { toast } from 'react-toastify'
-import { Modal, Select } from "antd"
-import Pagepagination from "../../components/Pagination"
+import { Modal, Select, Table } from "antd"
 
 function Allproducts() {
 
@@ -14,8 +13,6 @@ function Allproducts() {
     const [loading, setLoading] = useState(true)
     const [Id, setId] = useState('')
     const { loginUser } = useContext(UserName)
-
-    let id = 1
 
     useEffect(() => {
         const fetchAllProducts = async () => {
@@ -59,173 +56,143 @@ function Allproducts() {
         )
     }
 
+    const columns = [
+        {
+            title: 'Product Name',
+            dataIndex: 'productName',
+            key: 'name'
+        },
+        {
+            title: 'Description',
+            dataIndex: 'productDescription',
+            key: 'description',
+        },
+        {
+            title: 'Description',
+            dataIndex: 'image',
+            key: 'image',
+            render: (text) => <img src={`http://localhost:3000/images/${text}`} className="h-14 w-14 rounded-lg" ></img>
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_) => (
+                <Select
+                    aria-readonly
+                    className="outline-none"
+                    placeholder='...'
+                    optionFilterProp="children"
+                    onChange={(value) => {
+
+                        if (value == 'edit') {
+                            
+                            navigate("/home/update-product", {
+                                state: {
+                                    id: _._id
+                                }
+                            })
+                        }
+
+                        if (value == 'delete') {
+                            debugger
+                            setIsModalOpen(true)
+                            setId(_._id)
+                        }
+
+                        if (value == 'view') {
+
+                            const viewProduct = async () => {
+                                debugger
+                                const { data } = await axios.get(`http://localhost:3000/product-data?productId=${_._id}`)
+
+                                if (data) {
+                                    navigate("/home/view-product", {
+                                        state: {
+                                            data: _
+                                        }
+                                    })
+                                }
+                            }
+                            viewProduct()
+                        }
+                    }}
+                    style={{ width: "80px" }}
+                    options={[
+                        {
+                            value: 'edit',
+                            label: 'Edit',
+                        },
+                        {
+                            value: 'delete',
+                            label: 'Delete',
+                        },
+                        {
+                            value: 'view',
+                            label: 'View',
+                        },
+                    ]}
+                />
+            ),
+        },
+    ]
+
+    const datasource = products.map((product, index) => {
+        return {
+            ...product,
+            key: index + 1
+        }
+    })
+
     return (
         <>
-            {products.length > 0
+            {datasource.length > 0
                 ? <h1 className="text-center text-4xl font-extrabold mb-5">
                     All products
                 </h1>
                 : <h1 className="text-center font-extrabold text-3xl">No products found</h1>
             }
-            <div className="grid grid-cols-1">
-                <table className="h-full table-fixed border border-black text-center">
-                    <thead>
-                        <tr>
-                            <th>id</th>
-                            <th>product-name</th>
-                            <th>product-description</th>
-                            <td>Image</td>
-                            <th>action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            products.map(product => (
-                                <tr key={product._id} className="border border-black ">
-                                    <td>{id++}</td>
-                                    <td>{product.productName}</td>
-                                    <td>{product.productDescription}</td>
-                                    <td>
-                                        <div className="h-16 w-16 flex items-center justify-center mx-auto">
-                                            <img className="h-14 rounded-md w-14" src={`http://localhost:3000/images/${product.image}`} alt="image" />
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div>
-                                            <Modal
-                                                style={{
-                                                    height: "800px"
-                                                }}
-                                                open={isModalOpen}
-                                                onHide={() => {
-                                                    setIsModalOpen(false)
-                                                    setLoading(true)
-                                                }}
-                                                dialogClassName="modal-90w"
-                                                aria-labelledby="contained-modal-title-vcenter"
-                                                onOk={isOk}
-                                                okButtonProps={{ style: { backgroundColor: "green" } }}
-                                                onCancel={() => {
-                                                    setIsModalOpen(false)
-                                                    setLoading(true)
-                                                }
-                                                }
-                                                cancelButtonProps={{
-                                                    style: {
-                                                        backgroundColor: "red",
-                                                        color: "white"
-                                                    }
-                                                }}
-                                            >
-                                                <p className="text-3xl text-center">Are you sure ?</p>
-                                                <p className="text-center">You want to delete post?</p>
-                                            </Modal>
-                                            <Select
-                                                aria-readonly
-                                                className="outline-none"
-                                                placeholder='...'
-                                                optionFilterProp="children"
-                                                onChange={(value) => {
-
-                                                    if (value == 'edit') {
-
-                                                        navigate("/home/update-product", {
-                                                            state: {
-                                                                id: product._id
-                                                            }
-                                                        })
-                                                    }
-
-                                                    if (value == 'delete') {
-                                                        debugger
-                                                        setIsModalOpen(true)
-                                                        setId(product._id)
-                                                    }
-
-                                                    if (value == 'view') {
-
-                                                        const viewProduct = async () => {
-                                                            debugger
-                                                            const { data } = await axios.get(`http://localhost:3000/product-data?productId=${product._id}`)
-
-                                                            if (data) {
-                                                                navigate("/home/view-product", {
-                                                                    state: {
-                                                                        data: data
-                                                                    }
-                                                                })
-                                                            }
-                                                        }
-                                                        viewProduct()
-                                                    }
-                                                }}
-                                                style={{ width: "80px" }}
-                                                options={[
-                                                    {
-                                                        value: 'edit',
-                                                        label: 'Edit',
-                                                    },
-                                                    {
-                                                        value: 'delete',
-                                                        label: 'Delete',
-                                                    },
-                                                    {
-                                                        value: 'view',
-                                                        label: 'View',
-                                                    },
-                                                ]}
-                                            />
-                                        </div>
-                                    </td>
-                                    {/* <td><button className="bg-gray-500 text-white px-4 py-2 mt-1 mb-1 rounded-lg" onClick={(e) => {
-                                        e.preventDefault()
-
-                                        navigate("/home/update-product", {
-                                            state: {
-                                                id: product._id
-                                            }
-                                        })
-                                    }}>Edit</button></td>
-                                    <td><button className="bg-red-700 text-white px-4 py-2 mt-1 mb-1 rounded-lg" onClick={() => {
-
-                                        const deleteData = async () => {
-                                            try {
-                                                debugger
-                                                const { data } = await axios.delete(`http://localhost:3000/delete-product?id=${product._id}`)
-
-                                                if (data.message) {
-                                                    toast.success("product deleted")
-                                                }
-                                            } catch (error) {
-                                                console.log(error);
-                                            }
-                                        }
-                                        deleteData()
-                                        setLoading(true)
-                                    }}>Delete</button></td>
-                                    <td><button className="bg-green-600 text-white px-4 py-2 mt-1 mb-1 rounded-lg" onClick={() => {
-                                        const viewProduct = async () => {
-                                            debugger
-                                            const { data } = await axios.get(`http://localhost:3000/product-data?productId=${product._id}`)
-
-                                            if (data) {
-                                                navigate("/home/view-product",{
-                                                    state:{
-                                                        data:data
-                                                    }
-                                                })
-                                            }
-                                        }
-                                        viewProduct()
-                                    }}>View</button></td> */}
-                                </tr>
-                            ))
+            {
+                <div>
+                <Modal
+                    style={{
+                        height: "800px"
+                    }}
+                    open={isModalOpen}
+                    onHide={() => {
+                        setIsModalOpen(false)
+                        setLoading(true)
+                    }}
+                    dialogClassName="modal-90w"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    onOk={isOk}
+                    okButtonProps={{ style: { backgroundColor: "green" } }}
+                    onCancel={() => {
+                        setIsModalOpen(false)
+                        setLoading(true)
+                    }
+                    }
+                    cancelButtonProps={{
+                        style: {
+                            backgroundColor: "red",
+                            color: "white"
                         }
-                    </tbody>
-                </table>
+                    }}
+                >
+                    <p className="text-3xl text-center">Are you sure ?</p>
+                    <p className="text-center">You want to delete post?</p>
+                </Modal>
+                
             </div>
-
-            <Pagepagination />
+            }
+            <Table 
+                dataSource={datasource} 
+                columns={columns} 
+                pagination={{
+                    pageSize:1,
+                    total:products.length,
+                    defaultCurrent:1
+                }}
+            />
         </>
 
     )
